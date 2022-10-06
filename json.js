f121feec713507baa48eac760cb31371bf32aa35
aatarsh <aatarshkm@gmail.com>
2022-10-06 15:15:54 +0530

    commit2

---
+++ json.js
@@ -0,0 +1,85 @@
+const data=[{
+    AWBNumber:618-4274283,
+    CustomerName:"Aatarsh",
+    SHC:"VAL EAP EAW",
+    HandlingArea:"AFT1",
+    ExportImport:"Import",
+    ChargeAdvice:{
+        WeightVerification:{
+            Quantity:1000.0,
+            Duration:null,
+            Amount:3.00,
+            CollectionMechanism:["BILL","COLLECT"],
+            paid:false,
+            recieptNumber:null,
+            waivedAmount:null
+            
+        },
+        RcarScreeningFee:{
+            Quantity:1000.0,
+            Duration:null,
+            Amount:90.00,
+            CollectionMechanism:["BILL","COLLECT"],
+            paid:false,
+            recieptNumber:null,
+            waivedAmount:null
+            
+        },
+        FwbCreationServiceFee:{
+            Quantity:1.0,
+            Duration:null,
+            Amount:18.00,
+            CollectionMechanism:"Collect",
+            paid:false,
+            recieptNumber:null,
+            waivedAmount:null
+            
+        },
+        DangerousGoodsHandlingFee:{
+            Quantity:10.0,
+            Duration:null,
+            Amount:100.00,
+            CollectionMechanism:"Collect",
+            paid:true,
+            recieptNumber:"100-09102917",
+            waivedAmount:null
+            
+        },
+        ForkLiftCharges:{
+            Quantity:null,
+            Duration:3,
+            Amount:360.00,
+            CollectionMechanism:"Collect",
+            paid:true,
+            recieptNumber:"100-09102917",
+            waivedAmount:null
+            
+        },
+        ToCollect:{
+            Quantity:null,
+            Duration:null,
+            Amount:1378.00,
+            CollectionMechanism:null,
+            paid:null,
+            recieptNumber:null,
+            waivedAmount:null
+            
+        },
+        ToBill:{
+            Quantity:null,
+            Duration:null,
+            Amount:0.00,
+            CollectionMechanism:null,
+            paid:null,
+            recieptNumber:null,
+            waivedAmount:null
+            
+        },
+        
+        
+
+
+    }
+
+
+}]
