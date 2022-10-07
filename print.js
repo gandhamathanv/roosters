57f890d271a5a990a9924b0a5e155147d8edae85
DINESHBABU R <90496628+DINESHBABU2002@users.noreply.github.com>
2022-10-07 12:33:18 +0530

    file added

--- print.js
+++
@@ -1,9 +0,0 @@
-function take_print() {
-  console.log("take print");
-
-  const doc = new jsPDF("p", "pt", "a4");
-  let source = document.querySelector(".table").innerHTML;
-
-  doc.fromHTML(source);
-  doc.save("table.pdf");
-}
