8c528b3c8a1a13f41fcc37007429be53d89f5d45
gandhamathan <gandhasaroja@gmail.com>
2022-10-08 09:52:52 +0530

    container

--- waive-approve.js
+++ waive-approve.js
@@ -9,6 +9,7 @@ const dataContent = [{
         statusDate: "",
         status: "PENDING",
         approverRemark: "",
+        readOnly: false,
     },
     {
         AWBNumber: "616-01001002",
@@ -20,6 +21,7 @@ const dataContent = [{
         statusDate: "06OCT2022 20:35",
         status: "APPROVED",
         approverRemark: "",
+        readOnly: false,
     },
     {
         AWBNumber: "616-01001003",
@@ -31,6 +33,7 @@ const dataContent = [{
         statusDate: "05OCT2022 18:10",
         status: "REJECTED",
         approverRemark: "TOO MUCH",
+        readOnly: false,
     },
     {
         AWBNumber: "616-01001007",
@@ -42,6 +45,7 @@ const dataContent = [{
         statusDate: "10OCT2022 11:32",
         status: "UNDECIDED",
         approverRemark: "LEFT UNDECIDED",
+        readOnly: false,
     },
 ];
 
@@ -66,159 +70,187 @@ const content = (el) => ` <tr>
 <td>${el.waiveAmount}</td>
 <td>${el.waiveBy}</td>
 <td>${el.waiveDate}</td>
+${
+  !el.readOnly
+    ? `
+<td style="background-color:${
+        el.status == "PENDING"
+          ? "yellow"
+          : el.status == "APPROVED"
+          ? "green"
+          : el.status == "REJECTED"
+          ? "red"
+          : el.status == "UNDECIDED"
+          ? "grey"
+          : "white"
+      }">${el.status}</td>`
+    : `
+<td>
+edit</td>`
+}
 <td>${el.approver}</td>
 <td>${el.statusDate}</td>
-<td style="background-color:${
-  el.status == "PENDING"
-    ? "yellow"
-    : el.status == "APPROVED"
-    ? "green"
-    : el.status == "REJECTED"
-    ? "red"
-    : el.status == "UNDECIDED"
-    ? "grey"
-    : "white"
-}">${el.status}</td>
-<td><input class="table-text-input" type="text" value="${
-  el.approverRemark
-}" /></td>
+<td>
+${
+  el.readOnly
+    ? `<input class="table-text-input" type="text" value="${el.approverRemark}" />`
+    : el.approverRemark
+}
+</td>
+    
+
+<td><i class="fa-solid fa-pen edit-tabel"></i></td>
 </tr>`;
 
 // SET DATA FOR UI FUNCTION
 const setData = (data) => {
-    tabelContent.innerHTML = "";
-    data.forEach((el) => {
-        tabelContent.innerHTML += content(el);
-    });
+  tabelContent.innerHTML = "";
+  data.forEach((el) => {
+    tabelContent.innerHTML += content(el);
+  });
 };
 // SUB FUNCTIONS
 
 const sortString = (a, b, value) => {
-    let fa = a[value].toLowerCase(),
-        fb = b[value].toLowerCase();
+  let fa = a[value].toLowerCase(),
+    fb = b[value].toLowerCase();
 
-    if (fa < fb) {
-        return -1;
-    }
-    if (fa > fb) {
-        return 1;
-    }
-    return 0;
+  if (fa < fb) {
+    return -1;
+  }
+  if (fa > fb) {
+    return 1;
+  }
+  return 0;
 };
 const sortNumber = (a, b, value) => b[value] - a[value];
 
 const sortDate = (a, b, value) => {
-    let da = new Date(a[value]),
-        db = new Date(b[value]);
-    return da - db;
+  let da = new Date(a[value]),
+    db = new Date(b[value]);
+  return da - db;
 };
 
 // SORT DATA FUNCTION
 const sortData = (data, index) => {
-    switch (index) {
-        case 0:
-            data.sort((a, b) => sortString(a, b, "AWBNumber"));
-            break;
-        case 1:
-            data.sort((a, b) => sortNumber(a, b, "actualAmount"));
-            break;
-        case 2:
-            data.sort((a, b) => sortNumber(a, b, "waiveAmount"));
-            break;
-        case 3:
-            data.sort((a, b) => sortString(a, b, "waiveBy"));
-            break;
-        case 4:
-            data.sort((a, b) => sortDate(a, b, "waiveDate"));
-            break;
-        case 5:
-            data.sort((a, b) => sortString(a, b, "approver"));
-            break;
-        case 6:
-            data.sort((a, b) => sortDate(a, b, "statusDate"));
-            break;
-        case 7:
-            data.sort((a, b) => sortString(a, b, "status"));
+  switch (index) {
+    case 0:
+      data.sort((a, b) => sortString(a, b, "AWBNumber"));
+      break;
+    case 1:
+      data.sort((a, b) => sortNumber(a, b, "actualAmount"));
+      break;
+    case 2:
+      data.sort((a, b) => sortNumber(a, b, "waiveAmount"));
+      break;
+    case 3:
+      data.sort((a, b) => sortString(a, b, "waiveBy"));
+      break;
+    case 4:
+      data.sort((a, b) => sortDate(a, b, "waiveDate"));
+      break;
+    case 5:
+      data.sort((a, b) => sortString(a, b, "approver"));
+      break;
+    case 6:
+      data.sort((a, b) => sortDate(a, b, "statusDate"));
+      break;
+    case 7:
+      data.sort((a, b) => sortString(a, b, "status"));
 
-            break;
-    }
-    if (userVariables.sortTabel == null || userVariables.sortTabel == "DESC") {
-        setData(data);
-        userVariables.sortTabel = "ASC";
-    } else if (userVariables.sortTabel == "ASC") {
-        setData(data.reverse());
-        userVariables.sortTabel = "DESC";
-    }
-    // console.log(dsata);
+      break;
+  }
+  if (userVariables.sortTabel == null || userVariables.sortTabel == "DESC") {
+    setData(data);
+    userVariables.sortTabel = "ASC";
+  } else if (userVariables.sortTabel == "ASC") {
+    setData(data.reverse());
+    userVariables.sortTabel = "DESC";
+  }
+  // console.log(dsata);
 };
 
 // FILTER FUNCTION
 const filterData = (data) => {
-    const searchNumber = AWBNumber.value;
-    const searchName = customerName.value;
-    let searchFrom = dateFrom.value;
-    let searchTo = dateTo.value;
-    console.log(searchNumber, searchName, searchFrom, searchTo);
-    if (searchNumber != "") {
-        data = data.filter((el) => {
-            return el.AWBNumber.startsWith(searchNumber);
-        });
-    }
-    if (searchName != "") {
-        data = data.filter((el) => {
-            console.log(searchName, el.customerName);
-            return el.waiveBy.toLowerCase().startsWith(searchName.toLowerCase());
-        });
-    }
-    if (searchFrom != "" && searchTo != "") {
-        searchFrom = new Date(searchFrom);
-        searchTo = new Date(searchTo);
-        data = data.filter((el) => {
-            const waiveDate = new Date(el.waiveDate);
-            const approveDate = new Date(el.approveDate);
-            return (
-                (waiveDate >= searchFrom && waiveDate <= searchTo) ||
-                (approveDate >= searchFrom && approveDate <= searchTo)
-            );
-        });
-    } else if (searchFrom != "" && searchTo == "") {
-        searchFrom = new Date(searchFrom);
-        data = data.filter((el) => {
-            const waiveDate = new Date(el.waiveDate);
-            const approveDate = new Date(el.approveDate);
-            return waiveDate >= searchFrom || approveDate >= searchFrom;
-        });
-    } else if (searchFrom == "" && searchTo != "") {
-        searchTo = new Date(searchTo);
-        data = data.filter((el) => {
-            const waiveDate = new Date(el.waiveDate);
-            const approveDate = new Date(el.approveDate);
-            return waiveDate <= searchTo || approveDate <= searchTo;
-        });
-    }
+  const searchNumber = AWBNumber.value;
+  const searchName = customerName.value;
+  let searchFrom = dateFrom.value;
+  let searchTo = dateTo.value;
+  console.log(searchNumber, searchName, searchFrom, searchTo);
+  if (searchNumber != "") {
+    data = data.filter((el) => {
+      return el.AWBNumber.startsWith(searchNumber);
+    });
+  }
+  if (searchName != "") {
+    data = data.filter((el) => {
+      console.log(searchName, el.customerName);
+      return el.waiveBy.toLowerCase().startsWith(searchName.toLowerCase());
+    });
+  }
+  if (searchFrom != "" && searchTo != "") {
+    searchFrom = new Date(searchFrom);
+    searchTo = new Date(searchTo);
+    data = data.filter((el) => {
+      const waiveDate = new Date(el.waiveDate);
+      const approveDate = new Date(el.approveDate);
+      return (
+        (waiveDate >= searchFrom && waiveDate <= searchTo) ||
+        (approveDate >= searchFrom && approveDate <= searchTo)
+      );
+    });
+  } else if (searchFrom != "" && searchTo == "") {
+    searchFrom = new Date(searchFrom);
+    data = data.filter((el) => {
+      const waiveDate = new Date(el.waiveDate);
+      const approveDate = new Date(el.approveDate);
+      return waiveDate >= searchFrom || approveDate >= searchFrom;
+    });
+  } else if (searchFrom == "" && searchTo != "") {
+    searchTo = new Date(searchTo);
+    data = data.filter((el) => {
+      const waiveDate = new Date(el.waiveDate);
+      const approveDate = new Date(el.approveDate);
+      return waiveDate <= searchTo || approveDate <= searchTo;
+    });
+  }
 
-    console.log(data);
-    return data;
+  console.log(data);
+  return data;
 };
 
 // ADD EVENT LISTENER
 // SEARCH BUTTON
 searchButton.addEventListener("click", (el) => {
-    setData(filterData(dataContent));
+  setData(filterData(dataContent));
+  setEdit(document.querySelectorAll(".edit-tabel"));
 });
 // SORT BUTTON
 for (var i = 0; i < sortButton.length; i++) {
-    (function(index) {
-        sortButton[index].addEventListener("click", function() {
-            sortData(dataContent, index);
-        });
-    })(i);
+  (function (index) {
+    sortButton[index].addEventListener("click", function () {
+      sortData(dataContent, index);
+    });
+  })(i);
 }
 
+const setEdit = (editButton) => {
+  for (var i = 0; i < editButton.length; i++) {
+    (function (index) {
+      editButton[index].addEventListener("click", function () {
+        console.log(dataContent[index].readOnly);
+        dataContent[index].readOnly = !dataContent[index].readOnly;
+        console.log(dataContent[index].readOnly);
+        console.log(dataContent[index]);
+        setData(dataContent);
+      });
+    })(i);
+  }
+};
 // CALLING UI FUNCTION
 
 setData(dataContent);
-console.log(sortButton);
+setEdit(document.querySelectorAll(".edit-tabel"));
 
 console.log(new Date(dataContent[0].waiveDate));
 
@@ -229,9 +261,9 @@ console.log(new Date(dataContent[0].waiveDate));
 // PRINT tablE
 
 function printData() {
-    var divToPrint = document.getElementById("main-table");
-    newWin = window.open("");
-    newWin.document.write(divToPrint.outerHTML);
-    newWin.print();
-    newWin.close();
+  var divToPrint = document.getElementById("main-table");
+  newWin = window.open("");
+  newWin.document.write(divToPrint.outerHTML);
+  newWin.print();
+  newWin.close();
 }
